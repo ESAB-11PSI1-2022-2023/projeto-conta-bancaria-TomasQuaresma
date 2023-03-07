9943b360315d67fc635e35d7bc2a11cb292f076a
TomasQuaresma <114538426+TomasQuaresma@users.noreply.github.com>
2023-03-07 17:53:10 +0000

    Adicionado Design ao  Form1

--- TrabalhoContaBancaria/Form1.Designer.cs
+++ TrabalhoContaBancaria/Form1.Designer.cs
@@ -28,13 +28,232 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Numero = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Quantia = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Deposito = new System.Windows.Forms.Button();
+            this.Levantamento = new System.Windows.Forms.Button();
+            this.Fechar = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.Numero);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(434, 150);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Conta à Ordem";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(114, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(274, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Tintim";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(111, 115);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 16);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "10 000,00 €";
+            // 
+            // Numero
+            // 
+            this.Numero.AutoSize = true;
+            this.Numero.Location = new System.Drawing.Point(111, 20);
+            this.Numero.Name = "Numero";
+            this.Numero.Size = new System.Drawing.Size(0, 16);
+            this.Numero.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(44, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Saldo :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(44, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "&Titular :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Número :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Quantia);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.Deposito);
+            this.groupBox2.Controls.Add(this.Levantamento);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 240);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(434, 150);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Operações";
+            // 
+            // Quantia
+            // 
+            this.Quantia.Location = new System.Drawing.Point(107, 70);
+            this.Quantia.Name = "Quantia";
+            this.Quantia.Size = new System.Drawing.Size(143, 22);
+            this.Quantia.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "€";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "&Quantia :";
+            // 
+            // Deposito
+            // 
+            this.Deposito.Location = new System.Drawing.Point(163, 21);
+            this.Deposito.Name = "Deposito";
+            this.Deposito.Size = new System.Drawing.Size(100, 25);
+            this.Deposito.TabIndex = 1;
+            this.Deposito.Text = "&Depósito";
+            this.Deposito.UseVisualStyleBackColor = true;
+            this.Deposito.Click += new System.EventHandler(this.Deposito_Click);
+            // 
+            // Levantamento
+            // 
+            this.Levantamento.Location = new System.Drawing.Point(44, 21);
+            this.Levantamento.Name = "Levantamento";
+            this.Levantamento.Size = new System.Drawing.Size(113, 25);
+            this.Levantamento.TabIndex = 0;
+            this.Levantamento.Text = "&Levantamento";
+            this.Levantamento.UseVisualStyleBackColor = true;
+            this.Levantamento.Click += new System.EventHandler(this.Levantamento_Click);
+            // 
+            // Fechar
+            // 
+            this.Fechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Fechar.Location = new System.Drawing.Point(371, 406);
+            this.Fechar.Name = "Fechar";
+            this.Fechar.Size = new System.Drawing.Size(75, 25);
+            this.Fechar.TabIndex = 2;
+            this.Fechar.Text = "&Fechar";
+            this.Fechar.UseVisualStyleBackColor = true;
+            this.Fechar.Click += new System.EventHandler(this.Fechar_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 33);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(118, 16);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Contas bancárias :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AB0001-661",
+            "0658187233661",
+            "UT0023-110"});
+            this.comboBox1.Location = new System.Drawing.Point(136, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(310, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(458, 443);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Fechar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Banco";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label Numero;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox Quantia;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Deposito;
+        private System.Windows.Forms.Button Levantamento;
+        private System.Windows.Forms.Button Fechar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- TrabalhoContaBancaria/Form1.cs
+++ TrabalhoContaBancaria/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright(c) Tomás Quaresma. All rights reserved.
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,63 @@ namespace TrabalhoContaBancaria
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Botão Fechar
+        /// </summary>
+        private void Fechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Botão Levantamento, retira a quantidade de saldo escolhida pelo utilizador
+        /// </summary>
+        private void Levantamento_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt32(Quantia.Text) < 0)
+                {
+                    MessageBox.Show("Introduza um valor numérico valido.","Erro",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    Quantia.Text = null;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Introduza um valor numérico valido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Quantia.Text = null;
+            }
+        }
+
+        /// <summary>
+        /// Botão Deposito, adiciona a quantidade de saldo escolhida pelo utilizador
+        /// </summary>
+        private void Deposito_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt32(Quantia.Text) < 0)
+                {
+                    MessageBox.Show("Introduza um valor numérico valido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Quantia.Text = null;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Introduza um valor numérico valido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Quantia.Text = null;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Numero.Text = comboBox1.SelectedItem.ToString();
         }
     }
 }
