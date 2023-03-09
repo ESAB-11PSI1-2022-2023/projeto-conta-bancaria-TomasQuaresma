cab33110e6350baa585813619b54d80e65cd72b4
TomasQuaresma <114538426+TomasQuaresma@users.noreply.github.com>
2023-03-09 15:18:02 +0000

    Criação de um novo form : "Login"
    Foi criado a form "Login", foram tiradas as bordas do form e foi adicionado um código para  ser possível move-lo mesmo não tendo borda

---
+++ TrabalhoContaBancaria/Login.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabalhoContaBancaria
+{
+    public partial class Login : Form
+    {
+        int X = 0;
+        int Y = 0;
+        public Login()
+        {
+            InitializeComponent();
+            //Para Mover o Form sem ter borda
+            this.MouseDown += new MouseEventHandler(Login_MouseDown);
+            this.MouseMove += new MouseEventHandler(Login_MouseMove);
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Codigo para conseguir mover o form mesmo não tendo borda
+        /// </summary>
+        private void Login_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+            X = this.Left - MousePosition.X;
+            Y = this.Top - MousePosition.Y;
+
+        }
+
+        private void Login_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+            this.Left = X + MousePosition.X;
+            this.Top = Y + MousePosition.Y;
+        }
+    }
+}
